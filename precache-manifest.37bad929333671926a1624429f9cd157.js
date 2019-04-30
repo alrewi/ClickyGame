43b980d2d724ae96962dacf70275391105b12a77
Alex <alexandrareginawilson@gmail.com>
2019-04-30 13:04:34 -0500

    Updates

---
+++ precache-manifest.37bad929333671926a1624429f9cd157.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0655fc17ba587c923d79de34be8eacd8",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "967c2cc9fc1686b4cf8d",
+    "url": "/ClickyGame/static/css/main.4506351c.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/ClickyGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "967c2cc9fc1686b4cf8d",
+    "url": "/ClickyGame/static/js/main.7d5800f7.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
